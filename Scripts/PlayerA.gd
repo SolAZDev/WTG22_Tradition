4fb3485e25587f4d86cbf07e92e0080dc459ea1a
Bryan B. Bonilla Díaz <45500800+lunzyde@users.noreply.github.com>
2022-07-30 02:32:49 -0400

    Debugging Commands

--- Scripts/PlayerA.gd
+++ Scripts/PlayerA.gd
@@ -2,6 +2,8 @@ extends Actor
 
 const bulletPath = preload('res://Scenes/Bullet.tscn')
 var bulletAmmo = 5
+var playerHealth = 5
+var IsPlayerDead = false
 
 #export(NodePath) var bulletPath
 
@@ -11,7 +13,7 @@ func _ready():
 	pass # Replace with function body.
 	
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("attack"):
 		if bulletAmmo > 0:
 			bulletAmmo = bulletAmmo - 1
 		else:
@@ -19,8 +21,30 @@ func _process(delta):
 		
 		print('SHOOTING! Ammo: ' , bulletAmmo)
 		shoot()
-		
+	
+	if Input.is_action_just_pressed("debug_ammoplus"):
+		bulletAmmo = bulletAmmo + 1
+		print('Bullets Increased! Ammo: ' , bulletAmmo)
+	
+	if Input.is_action_just_pressed("debug_heal"):
+		playerHealth = playerHealth + 1
+		print('Health Increased! Life: ' , playerHealth)
+	
+	if Input.is_action_just_pressed("debug_hurt"):
+		playerHealth = playerHealth - 1
+		print('Health Decreased! Life: ' , playerHealth)
+	
 	look_at(get_global_mouse_position())
+	
+	if playerHealth == 0 || playerHealth < 0:
+		IsPlayerDead = true	
+	else:
+		IsPlayerDead = false
+	
+	if IsPlayerDead == true:
+		speed = 0
+	else:
+		speed = 200
 
 func shoot():
 	if bulletAmmo > 0:
@@ -30,7 +54,7 @@ func shoot():
 		bullet.Velocity = get_global_mouse_position() - position
 
 func _physics_process(delta):
-	vel = Vector2(Input.get_axis("ui_left","ui_right"),Input.get_axis("ui_up","ui_down"));
+	vel = Vector2(Input.get_axis("mv_left","mv_right"),Input.get_axis("mv_up","mv_down"));
 	move_and_slide(vel*speed)
 	
 
