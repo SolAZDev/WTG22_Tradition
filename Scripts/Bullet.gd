fee644924ce8d4b9fb7d3db38e44710735bbf420
SolAZDev <SolAZDev@Gmail.com>
2022-07-30 02:53:49 -0300

    AreaBullet Pt1

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,7 +1,11 @@
-extends KinematicBody2D
+extends Area2D
 
 var Velocity = Vector2(1,0)
 var Speed = 500
 
-func _physics_process(delta):
-	move_and_collide(Velocity.normalized() * delta * Speed)
+func _process(delta):
+	translate(Velocity*Speed)
+	
+	
+#func _physics_process(delta):
+#	move_and_collide(Velocity.normalized() * delta * Speed)
