c3c4a6a55bcc3cce146a149ad255fd55a3d9b39d
SolAZDev <SolAZDev@Gmail.com>
2022-07-30 03:42:46 -0300

    bullet stop

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,11 +1,24 @@
 extends Area2D
 
 var Velocity = Vector2(1,0)
-var Speed = 500
+export(float) var Speed = 2
+export(float) var SpeedOverTime=.25
+export(bool) var enableNeagtive=false;
+var shooting:bool = true
+
+onready var trueSpeed=Speed;
 
 func _process(delta):
-	translate(Velocity*Speed)
+	if shooting:
+		trueSpeed+=SpeedOverTime
+		if !enableNeagtive && trueSpeed<=0: trueSpeed=0
+		translate(Velocity*trueSpeed)
 	
+func _body_entered(node):
+	print(node, "?")
+
+func _on_Disk_body_entered(node:Node2D):
+	print(node.name,"!")
+	shooting=false
+	#play hit animation?
 	
-#func _physics_process(delta):
-#	move_and_collide(Velocity.normalized() * delta * Speed)
