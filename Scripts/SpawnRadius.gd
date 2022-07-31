08c42f856a25730193ddc62868d4cd2bef56263d
Bryan B. Bonilla Díaz <45500800+lunzyde@users.noreply.github.com>
2022-07-31 00:20:50 -0400

    Create SpawnRadius.gd

---
+++ Scripts/SpawnRadius.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+onready var timer: Timer = $SpawnTimer
+
+func _on_Area2D_body_entered(body):
+	if body.name == 'Player':
+		print('Player Detected!')
+		timer.start()
+
+func _on_Area2D_body_exited(body):
+	if body.name == 'Player':
+		print('Player Left!')
+		timer.stop()
+
+func _on_SpawnTimer_timeout():
+	print('Timeout!')
